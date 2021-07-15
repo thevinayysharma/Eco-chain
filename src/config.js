fac252f80352322c5f3b5ceb5fb077ffab75bbff
Vinay Sharma <vinay02856@gmail.com>
2021-07-15 15:08:43 +0530

    commit: open keys

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// Your public key (also your wallet address, freely shareable)
+ "04844ec879f955b4a3496cfb6cd552f8328c44d2c3d67b2b283cfd8a49145a0adb34a1b42f16ae0f1a3c3631e81cac3bf9ca666c6c50e54b9d3a21ca25319706d5"
+
+// Your private key (keep this secret! To sign transactions)
+ "caebd7b96ec48eec04ddc0cb707bf28d0310c75aad8807f66c363b47747959f0"
